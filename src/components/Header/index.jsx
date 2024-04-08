4f357cc942cd63ad2262b3c12779abc66bac765e
Douglas Shinzato <shinzato.douglas@gmail.com>
2024-04-08 15:48:11 -0400

    profile update adjustments

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -1,21 +1,25 @@
 import { Container, Profile, Logout } from "./styles"
 import { RiShutDownLine } from "react-icons/ri"
+import { useAuth } from "../../hooks/auth"
+import { api } from "../../services/api"
 
 export function Header() {
+  const { signOut, user } = useAuth()
+  const avatarUrl = user.avatar
+    ? `${api.defaults.baseURL}/files/${user.avatar}`
+    : avatarPlaceholder
+
   return (
     <Container>
       <Profile to="/profile">
-        <img
-          src="https://github.com/douglasshinzato.png"
-          alt="foto do usuário"
-        />
+        <img src={avatarUrl} alt={`foto de ${user.name}`} />
         <div>
           <span>Bem-vindo</span>
-          <strong>Douglas Shinzato</strong>
+          <strong>{user.name}</strong>
         </div>
       </Profile>
 
-      <Logout>
+      <Logout onClick={signOut}>
         <RiShutDownLine />
       </Logout>
     </Container>
--- src/pages/Profile/index.jsx
+++ src/pages/Profile/index.jsx
@@ -1,11 +1,48 @@
+import { useState } from "react"
 import { FiArrowLeft, FiUser, FiMail, FiLock, FiCamera } from "react-icons/fi"
 import { Link } from "react-router-dom"
 import { Input } from "../../components/Input"
 import { Button } from "../../components/Button"
+import { useAuth } from "../../hooks/auth"
+
+import { api } from "../../services/api"
+import avatarPlaceholder from "../../assets/avatar_placeholder.svg"
 
 import { Container, Form, Avatar } from "./style"
 
 export function Profile() {
+  const { user, updateProfile } = useAuth()
+
+  const [name, setName] = useState(user.name)
+  const [email, setEmail] = useState(user.email)
+  const [oldPassword, setOldPassword] = useState("")
+  const [newPassword, setNewPassword] = useState("")
+
+  const avatarUrl = user.avatar
+    ? `${api.defaults.baseURL}/files/${user.avatar}`
+    : avatarPlaceholder
+
+  const [avatar, setAvatar] = useState(avatarUrl)
+  const [avatarFile, setAvatarFile] = useState(null)
+
+  async function handleUpdate() {
+    const user = {
+      name,
+      email,
+      old_password: oldPassword,
+      password: newPassword,
+    }
+    await updateProfile({ user, avatarFile })
+  }
+
+  function handleChangeAvatar(event) {
+    const file = event.target.files[0]
+    setAvatarFile(file)
+
+    const imagePreview = URL.createObjectURL(file)
+    setAvatar(imagePreview)
+  }
+
   return (
     <Container>
       <header>
@@ -15,21 +52,40 @@ export function Profile() {
       </header>
       <Form>
         <Avatar>
-          <img
-            src="https://github.com/douglasshinzato.png"
-            alt="Foto do usuário"
-          />
+          <img src={avatar} alt="Foto do usuário" />
           <label htmlFor="avatar">
             <FiCamera />
-            <input id="avatar" type="file" />
+            <input id="avatar" type="file" onChange={handleChangeAvatar} />
           </label>
         </Avatar>
-        <Input placeholder="Nome" type="text" icon={FiUser} />
-        <Input placeholder="E-mail" type="text" icon={FiMail} />
-        <Input placeholder="Senha atual" type="password" icon={FiLock} />
-        <Input placeholder="Nova senha" type="password" icon={FiLock} />
+        <Input
+          placeholder="Nome"
+          type="text"
+          icon={FiUser}
+          value={name}
+          onChange={(event) => setName(event.target.value)}
+        />
+        <Input
+          placeholder="E-mail"
+          type="text"
+          icon={FiMail}
+          value={email}
+          onChange={(event) => setEmail(event.target.value)}
+        />
+        <Input
+          placeholder="Senha atual"
+          type="password"
+          icon={FiLock}
+          onChange={(event) => setOldPassword(event.target.value)}
+        />
+        <Input
+          placeholder="Nova senha"
+          type="password"
+          icon={FiLock}
+          onChange={(event) => setNewPassword(event.target.value)}
+        />
 
-        <Button title="Salvar" />
+        <Button title="Salvar" onClick={handleUpdate} />
       </Form>
     </Container>
   )
